c266a361cf4c99d842d91e23f23cd8df2eb71541
KrishnaInvestment <krishna.khatiwada187@gmail.com>
2023-07-22 02:18:45 +0545

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-VERSION = "0.0.1"
-DESCRIPTION = "AlgoAPI"
-LONG_DESCRIPTION = "Python project for Forexconnect API from FXCM"
+this_directory = Path(__file__).parent
+VERSION = "0.0.4"
+DESCRIPTION = "Python project for Forexconnect API from FXCM"
+LONG_DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 # Setting up
 setup(
@@ -10,8 +12,19 @@ setup(
     name="algoapi",
     version=VERSION,
     author="Krishna Khatiwada",
+    author_email="krishna.khatiwada187@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["forexconnect", "python-dotenv"],
+    install_requires=["forexconnect", "python-dotenv", "pandas==1.3.4"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "repository": "https://github.com/KrishnaInvestment/algoapi",
+    },
+    python_requires="<=3.8",
 )
